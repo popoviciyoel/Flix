8cff2b5199453d66d87db68c726b4d1eae2bb958
Yoel Popovici <popoviciyoel@gmail.com>
2021-09-07 23:05:24 -0400

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Yoel Popovici on 9/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
